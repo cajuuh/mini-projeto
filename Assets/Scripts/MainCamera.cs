671202f140b8cda48c3241c2b18bd92c6364d4bf
caiofelipe97 <caio.melo@embedded.ufcg.edu.br>
2016-07-08 16:09:59 -0300

    213911 Adciona spawn das plataformas

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -6,10 +6,14 @@ public class MainCamera : MonoBehaviour {
 	public Transform Player;
     public Transform endOfScreen;
 
-	// Use this for initialization
-	void Start () {
-		transform.position = new Vector3 (0, Player.position.y, -1);
+    // Use this for initialization
+    void Start()
+    {
+    }
+    void Update()
+    {
+        transform.position = new Vector3(0, Player.position.y, -1);
         endOfScreen.transform.position = new Vector3(0, Player.position.y, 0);
         //mantem o fim da tela junto ao player e a camera        
     }
-}
+   }
