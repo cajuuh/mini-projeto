c8ee49c11ad273f2654deea273d1ae9f7a8a8f6e
Pedro Alcântara <Pedro Alcntara>
2016-07-11 11:02:42 -0300

    #214244 - Personagem atravessa plataforma

---
+++ Assets/Scripts/PontudaScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PontudaScript : MonoBehaviour
+{
+
+    private readonly string PLAYER = "Player";
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == PLAYER)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
